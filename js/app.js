bd9d0680e857206a8d4287a52c023a641b637c3e
Ayush <singhayush484h@gmail.com>
2022-02-15 21:06:46 +0530

    initial

---
+++ js/app.js
@@ -0,0 +1,42 @@
+
+// const LISTIDmailchimp="bd6aca85bc";
+// const APIKEYmailchimp="fc7980d7d9a7a0c314c6a2b1850104a3-us14";
+// const APIKEYcoinmarket="23dc9522-f0c6-4c41-806b-c86e2e66359c"
+// const URLcoin="https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=23dc9522-f0c6-4c41-806b-c86e2e66359c"
+const URLmailchimp="https://us14.api.mailchimp.com/3.0/lists/bd6aca85bc";
+const options={method:"POST", auth:"user:fc7980d7d9a7a0c314c6a2b1850104a3-us14"};
+
+const express=require("express");
+const https=require("https");
+const path=require("path");
+const bodyParser=require("body-parser");
+const app=express();
+const PORT=2403;
+const request=https.request(URLmailchimp,options)
+
+app.use(express.static(path.join(__dirname,"../public")));
+app.use(bodyParser.urlencoded({ extended:true }));
+
+app.listen(2403);
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../public/html/index.html"))
+})
+
+app.post("/",(req,res)=>{
+
+    let data={
+        members:[
+            {
+                email_address:req.body.email,
+        merge_fields:{FNAME:req.body.firstname,LNAME:req.body.lastname},
+        status:"subscribed"
+            }
+        ]
+    }
+    data=JSON.stringify(data);
+  
+    request.write(data);
+    request.end();
+    res.send("ok");
+})
